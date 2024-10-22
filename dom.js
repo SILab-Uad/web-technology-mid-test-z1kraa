7b5bf843856f4bbe131d6813827cdc91756a13af
z1kraa <elekzikra@gmail.com>
2024-10-22 16:16:10 +0700

    update push file

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 // Dont change anything in this file
-
+import { generatePassword } from './script.js';
 document.getElementById('generateBtn').addEventListener('click', () => {
     const length = parseInt(document.getElementById('length').value, 10);
     const options = {
